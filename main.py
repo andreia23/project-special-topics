27f6142d55761c5d1c5b03b721b35ce376f6b062
Wellington Pessoa de Lima Filho <wellplf@gmail.com>
2021-09-02 08:05:54 -0300

    Almost Done!

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
 from treeD import Arvore
 from k_nN import KnN
+from datasets_ import Dataset
 from trainTest import Train_test
 import pandas as pd
 
-def main():
-    dataset = 'wine'
-    randomBase = Train_test(dataset)
-    y_position = 0
-    x1_position = 1
-    xn_position = 13
+#Parameters: Name, Y_position, Number of Attributes
+#Dataset("wine", "inicial", 13) 
+#Dataset("iris", "final", 4) 
+#Dataset("balance-scale", "inicial", 4) 
 
-    randomBase.eighty_by_twenty(y_position, x1_position, xn_position)
+def main():
+    dataset = Dataset("iris", "final", 4)
+    randomBase = Train_test(dataset)
+    randomBase.eighty_by_twenty()
 
     arvore_1 = Arvore(randomBase)
     arvore_1.treinamento_resultado('entropy')
     
-    arvore_2 = Arvore(randomBase)
-    arvore_2.treinamento_resultado('gini')
+    #arvore_2 = Arvore(randomBase)
+    #arvore_2.treinamento_resultado('gini')
 
-    knN_1 = KnN(randomBase)
-    knN_1.treinamento_resultado('euclidean', 3)
+    #knN_1 = KnN(randomBase)
+    #knN_1.treinamento_resultado('euclidean', 3)
 
-    knN_2 = KnN(randomBase)
-    knN_2.treinamento_resultado('euclidean', 3)
+    #knN_2 = KnN(randomBase)
+    #knN_2.treinamento_resultado('euclidean', 3)
     
-    knN_3 = KnN(randomBase)
-    knN_3.treinamento_resultado('euclidean', 3)
+    #knN_3 = KnN(randomBase)
+    #knN_3.treinamento_resultado('euclidean', 3)
     
-    knN_4 = KnN(randomBase)
-    knN_4.treinamento_resultado('manhattan', 3)
+    #knN_4 = KnN(randomBase)
+    #knN_4.treinamento_resultado('manhattan', 3)
 
-    knN_5 = KnN(randomBase)
-    knN_5.treinamento_resultado('manhattan', 3)
+    #knN_5 = KnN(randomBase)
+    #knN_5.treinamento_resultado('manhattan', 3)
 
-    knN_6 = KnN(randomBase)
-    knN_6.treinamento_resultado('manhattan', 3)
+    #knN_6 = KnN(randomBase)
+    #knN_6.treinamento_resultado('manhattan', 3)
 
     print(arvore_1)
-    print(arvore_2)
+    #print(arvore_2)
 
-    print(knN_1)
-    print(knN_2)
-    print(knN_3)
-    print(knN_4)
-    print(knN_5)
-    print(knN_6)
+    #print(knN_1)
+    #print(knN_2)
+    #print(knN_3)
+    #print(knN_4)
+    #print(knN_5)
+    #print(knN_6)
 
 if __name__ == '__main__':
     main()
--- trainTest.py
+++ trainTest.py
@@ -2,12 +2,12 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 class Train_test:
-    def __init__(self, filename, x_train = None, x_test = None, y_train = None, y_test = None):
+    def __init__(self, dataset, x_train = None, x_test = None, y_train = None, y_test = None):
         self._X_train = x_train
         self._X_test = x_test
         self._y_train = y_train
         self._y_test = y_test
-        self._filename = filename
+        self._dataset = dataset
 
     def get_x_train(self):
         return self._X_train
@@ -21,10 +21,19 @@ class Train_test:
     def get_y_test(self):
         return self._y_test
 
-    def eighty_by_twenty(self, index_Y, index_inicial, index_final):
-        arquivo = open(f"datasets/{self._filename}.data","r+")
+    def eighty_by_twenty(self):
+        arquivo = open(f"datasets/{self._dataset.getNome()}.data","r+")
     
         dataset = pd.read_csv(arquivo, header=None)
+
+        if(self._dataset.getYposition() == "inicial"):
+            index_Y = 0
+            index_inicial = 1
+            index_final = self._dataset.getLength()
+        else:
+            index_Y = self._dataset.getLength()
+            index_inicial = 0
+            index_final = index_Y - 1
 
         y = dataset[index_Y]
         X = dataset.loc[:,index_inicial:index_final]
