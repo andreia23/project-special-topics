080102e3c42bc57d2480e746349a675332e5c900
Wellington Pessoa de Lima Filho <wellplf@gmail.com>
2021-09-02 08:37:04 -0300

    Your move

--- main.py
+++ main.py
@@ -17,36 +17,36 @@ def main():
     arvore_1 = Arvore(randomBase)
     arvore_1.treinamento_resultado('entropy')
     
-    #arvore_2 = Arvore(randomBase)
-    #arvore_2.treinamento_resultado('gini')
+    arvore_2 = Arvore(randomBase)
+    arvore_2.treinamento_resultado('gini')
 
-    #knN_1 = KnN(randomBase)
-    #knN_1.treinamento_resultado('euclidean', 3)
+    knN_1 = KnN(randomBase)
+    knN_1.treinamento_resultado('euclidean', 5)
 
-    #knN_2 = KnN(randomBase)
-    #knN_2.treinamento_resultado('euclidean', 3)
+    knN_2 = KnN(randomBase)
+    knN_2.treinamento_resultado('euclidean', 10)
     
-    #knN_3 = KnN(randomBase)
-    #knN_3.treinamento_resultado('euclidean', 3)
+    knN_3 = KnN(randomBase)
+    knN_3.treinamento_resultado('euclidean', 15)
     
-    #knN_4 = KnN(randomBase)
-    #knN_4.treinamento_resultado('manhattan', 3)
+    knN_4 = KnN(randomBase)
+    knN_4.treinamento_resultado('manhattan', 5)
 
-    #knN_5 = KnN(randomBase)
-    #knN_5.treinamento_resultado('manhattan', 3)
+    knN_5 = KnN(randomBase)
+    knN_5.treinamento_resultado('manhattan', 10)
 
-    #knN_6 = KnN(randomBase)
-    #knN_6.treinamento_resultado('manhattan', 3)
+    knN_6 = KnN(randomBase)
+    knN_6.treinamento_resultado('manhattan', 15)
 
     print(arvore_1)
-    #print(arvore_2)
+    print(arvore_2)
 
-    #print(knN_1)
-    #print(knN_2)
-    #print(knN_3)
-    #print(knN_4)
-    #print(knN_5)
-    #print(knN_6)
+    print(knN_1)
+    print(knN_2)
+    print(knN_3)
+    print(knN_4)
+    print(knN_5)
+    print(knN_6)
 
 if __name__ == '__main__':
     main()
