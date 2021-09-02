398416c2baf24ea1fa0d21612b36a2e86d61a858
Wellington Pessoa de Lima Filho <wellplf@gmail.com>
2021-09-01 21:05:32 -0300

    Updated

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 from treeD import Arvore
+from K_nN import KnN
 from trainTest import Train_test
 import pandas as pd
 
 def main():
     randomBase = Train_test('iris')
-    randomBase.eighty_by_twenty(4, 0, 3)
+    y_position = 4
+    x1_position = 0
+    xn_position = 3
 
-    arvore_1 = Arvore(randomBase)
-    arvore_1.treinamento_resultado('entropy')
+    randomBase.eighty_by_twenty(y_position, x1_position, xn_position)
+
+    #arvore_1 = Arvore(randomBase)
+    #arvore_1.treinamento_resultado('entropy')
     
-    arvore_2 = Arvore(randomBase)
-    arvore_2.treinamento_resultado('gini')
+    #arvore_2 = Arvore(randomBase)
+    #arvore_2.treinamento_resultado('gini')
 
-    #knN_1 = KnN(randomBase, 'euclidian', 3)
-    #knN_2 = KnN(randomBase, 'euclidian', 3)
-    #knN_3 = KnN(randomBase, 'euclidian', 3)
-    #knN_4 = KnN(randomBase, 'vdm', 3)
-    #knN_5 = KnN(randomBase, 'vdm', 3)
-    #knN_6 = KnN(randomBase, 'vdm', 3)
+    knN_1 = KnN(randomBase)
+    knN_1.treinamento_resultado('euclidian', 3)
 
-    print(arvore_1)
-    print(arvore_2)
+    knN_2 = KnN(randomBase)
+    knN_2.treinamento_resultado('euclidian', 3)
+    
+    knN_3 = KnN(randomBase)
+    knN_3.treinamento_resultado('euclidian', 3)
+    
+    knN_4 = KnN(randomBase)
+    knN_4.treinamento_resultado('vdm', 3)
 
-    #print(knN_1)
+    knN_5 = KnN(randomBase)
+    knN_5.treinamento_resultado('vdm', 3)
+
+    knN_6 = KnN(randomBase)
+    knN_6.treinamento_resultado('vdm', 3)
+
+    #print(arvore_1)
+    #print(arvore_2)
+
+    print(knN_1)
     #print(knN_2)
     #print(knN_3)
     #print(knN_4)
